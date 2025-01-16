c9277eb23a282b3a5cacab4f754350320a9b8bc6
Chirag <chiragjadav134@gmail.com>
2025-01-16 15:02:19 +0530

    Initialize crypto app clone with basic structure and authentication setup

---
+++ app/signup.tsx
@@ -0,0 +1,117 @@
+import { Colors } from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ImageBackground,
+} from "react-native";
+const Page = () => {
+  const [countryCode, setCountryCode] = useState("+91");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 80 : 0;
+  const router = useRouter();
+  const { signUp } = useSignUp();
+
+  const onSignup = async () => {
+    const fullPhoneNumber = `${countryCode}${phoneNumber}`;
+
+    try {
+      await signUp!.create({
+        phoneNumber: fullPhoneNumber,
+      });
+      signUp!.preparePhoneNumberVerification();
+
+      router.push({
+        pathname: "/verify/[phone]",
+        params: { phone: fullPhoneNumber },
+      });
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1, justifyContent: "center" }}
+      behavior="padding"
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <ImageBackground
+        source={require("@/assets/images/sign_up.png")}
+        style={[
+          defaultStyles.container,
+          { justifyContent: "center", paddingVertical: 260 },
+        ]}
+      >
+        <Text style={defaultStyles.header}>Let's get started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send you a confirmation code there
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Country code"
+            placeholderTextColor={Colors.gray}
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="numeric"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+
+        <Link href={"/login"} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== "" ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignup}
+        >
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 12,
+    borderRadius: 18,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
+export default Page;
